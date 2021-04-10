891356731a95b87b8b8c9da9ce18d7deb33d4a5a
datavolunteer <datavolunteeryouness@gmail.com>
2021-04-10 18:57:07 +0200

    fix load image

--- dyslexia/preprocessing/__init__.py
+++ dyslexia/preprocessing/__init__.py
@@ -1,6 +1,6 @@
 __all__ = [
     'rotate_img', 'rgb2gray', 'image_to_gray', 'find_text_angle',
-    'remove_shadow'
+    'remove_shadow', 'binarize'
 ]
 
 from .preprocessing import rotate_img
@@ -8,3 +8,4 @@ from .preprocessing import rgb2gray
 from .preprocessing import image_to_gray
 from .preprocessing import find_text_angle
 from .preprocessing import remove_shadow
+from .preprocessing import binarize
